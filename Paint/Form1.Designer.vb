75cd106c2b89a28149c0e93a14927233f2e755ed
CCLC <cclc@lr.k12.nj.us>
2017-08-04 10:59:28 -0400

    Paint

---
+++ Paint/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Red = New System.Windows.Forms.Button()
+        Me.Orange = New System.Windows.Forms.Button()
+        Me.Yellow = New System.Windows.Forms.Button()
+        Me.LightGreen = New System.Windows.Forms.Button()
+        Me.DarkGreen = New System.Windows.Forms.Button()
+        Me.LightBlue = New System.Windows.Forms.Button()
+        Me.DarkBlue = New System.Windows.Forms.Button()
+        Me.Purple = New System.Windows.Forms.Button()
+        Me.Black = New System.Windows.Forms.Button()
+        Me.Eraser = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Line = New System.Windows.Forms.Button()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.BrushButton = New System.Windows.Forms.Button()
+        Me.OpenMenu = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "you Don't DARE!!!!!!!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Red
+        '
+        Me.Red.BackColor = System.Drawing.Color.Red
+        Me.Red.Location = New System.Drawing.Point(49, 99)
+        Me.Red.Name = "Red"
+        Me.Red.Size = New System.Drawing.Size(75, 23)
+        Me.Red.TabIndex = 4
+        Me.Red.Text = "Red"
+        Me.Red.UseVisualStyleBackColor = False
+        '
+        'Orange
+        '
+        Me.Orange.BackColor = System.Drawing.Color.OrangeRed
+        Me.Orange.Location = New System.Drawing.Point(49, 128)
+        Me.Orange.Name = "Orange"
+        Me.Orange.Size = New System.Drawing.Size(75, 23)
+        Me.Orange.TabIndex = 5
+        Me.Orange.Text = "Orange"
+        Me.Orange.UseVisualStyleBackColor = False
+        '
+        'Yellow
+        '
+        Me.Yellow.BackColor = System.Drawing.Color.Gold
+        Me.Yellow.Location = New System.Drawing.Point(49, 157)
+        Me.Yellow.Name = "Yellow"
+        Me.Yellow.Size = New System.Drawing.Size(75, 23)
+        Me.Yellow.TabIndex = 6
+        Me.Yellow.Text = "Yellow"
+        Me.Yellow.UseVisualStyleBackColor = False
+        '
+        'LightGreen
+        '
+        Me.LightGreen.BackColor = System.Drawing.Color.YellowGreen
+        Me.LightGreen.Location = New System.Drawing.Point(49, 186)
+        Me.LightGreen.Name = "LightGreen"
+        Me.LightGreen.Size = New System.Drawing.Size(75, 23)
+        Me.LightGreen.TabIndex = 7
+        Me.LightGreen.Text = "Light Green"
+        Me.LightGreen.UseVisualStyleBackColor = False
+        '
+        'DarkGreen
+        '
+        Me.DarkGreen.BackColor = System.Drawing.Color.Green
+        Me.DarkGreen.Location = New System.Drawing.Point(49, 215)
+        Me.DarkGreen.Name = "DarkGreen"
+        Me.DarkGreen.Size = New System.Drawing.Size(75, 23)
+        Me.DarkGreen.TabIndex = 8
+        Me.DarkGreen.Text = "Dark Green"
+        Me.DarkGreen.UseVisualStyleBackColor = False
+        '
+        'LightBlue
+        '
+        Me.LightBlue.BackColor = System.Drawing.Color.Cyan
+        Me.LightBlue.Location = New System.Drawing.Point(49, 244)
+        Me.LightBlue.Name = "LightBlue"
+        Me.LightBlue.Size = New System.Drawing.Size(75, 23)
+        Me.LightBlue.TabIndex = 9
+        Me.LightBlue.Text = "Light Blue"
+        Me.LightBlue.UseVisualStyleBackColor = False
+        '
+        'DarkBlue
+        '
+        Me.DarkBlue.BackColor = System.Drawing.Color.Blue
+        Me.DarkBlue.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.DarkBlue.Location = New System.Drawing.Point(49, 273)
+        Me.DarkBlue.Name = "DarkBlue"
+        Me.DarkBlue.Size = New System.Drawing.Size(75, 23)
+        Me.DarkBlue.TabIndex = 10
+        Me.DarkBlue.Text = "Dark Blue"
+        Me.DarkBlue.UseVisualStyleBackColor = False
+        '
+        'Purple
+        '
+        Me.Purple.BackColor = System.Drawing.Color.DarkViolet
+        Me.Purple.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Purple.Location = New System.Drawing.Point(49, 302)
+        Me.Purple.Name = "Purple"
+        Me.Purple.Size = New System.Drawing.Size(75, 23)
+        Me.Purple.TabIndex = 11
+        Me.Purple.Text = "Purple"
+        Me.Purple.UseVisualStyleBackColor = False
+        '
+        'Black
+        '
+        Me.Black.BackColor = System.Drawing.Color.Black
+        Me.Black.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Black.Location = New System.Drawing.Point(49, 331)
+        Me.Black.Name = "Black"
+        Me.Black.Size = New System.Drawing.Size(75, 23)
+        Me.Black.TabIndex = 12
+        Me.Black.Text = "Black"
+        Me.Black.UseVisualStyleBackColor = False
+        '
+        'Eraser
+        '
+        Me.Eraser.Location = New System.Drawing.Point(49, 360)
+        Me.Eraser.Name = "Eraser"
+        Me.Eraser.Size = New System.Drawing.Size(75, 23)
+        Me.Eraser.TabIndex = 13
+        Me.Eraser.Text = "Eraser"
+        Me.Eraser.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.Location = New System.Drawing.Point(49, 389)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(75, 23)
+        Me.Clear.TabIndex = 14
+        Me.Clear.Text = "Clear"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Line)
+        Me.Panel1.Controls.Add(Me.HScrollBar1)
+        Me.Panel1.Controls.Add(Me.BrushButton)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Red)
+        Me.Panel1.Controls.Add(Me.Clear)
+        Me.Panel1.Controls.Add(Me.Orange)
+        Me.Panel1.Controls.Add(Me.Eraser)
+        Me.Panel1.Controls.Add(Me.Yellow)
+        Me.Panel1.Controls.Add(Me.Black)
+        Me.Panel1.Controls.Add(Me.LightGreen)
+        Me.Panel1.Controls.Add(Me.Purple)
+        Me.Panel1.Controls.Add(Me.DarkGreen)
+        Me.Panel1.Controls.Add(Me.DarkBlue)
+        Me.Panel1.Controls.Add(Me.LightBlue)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel1.Location = New System.Drawing.Point(1015, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(140, 455)
+        Me.Panel1.TabIndex = 15
+        '
+        'Line
+        '
+        Me.Line.Location = New System.Drawing.Point(49, 70)
+        Me.Line.Name = "Line"
+        Me.Line.Size = New System.Drawing.Size(75, 23)
+        Me.Line.TabIndex = 17
+        Me.Line.Text = "Line"
+        Me.Line.UseVisualStyleBackColor = True
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Location = New System.Drawing.Point(3, 418)
+        Me.HScrollBar1.Maximum = 200
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(121, 28)
+        Me.HScrollBar1.TabIndex = 16
+        Me.HScrollBar1.Value = 2
+        '
+        'BrushButton
+        '
+        Me.BrushButton.Location = New System.Drawing.Point(49, 41)
+        Me.BrushButton.Name = "BrushButton"
+        Me.BrushButton.Size = New System.Drawing.Size(75, 23)
+        Me.BrushButton.TabIndex = 16
+        Me.BrushButton.Text = "Brush"
+        Me.BrushButton.UseVisualStyleBackColor = True
+        '
+        'OpenMenu
+        '
+        Me.OpenMenu.AutoSize = True
+        Me.OpenMenu.Location = New System.Drawing.Point(12, 9)
+        Me.OpenMenu.Name = "OpenMenu"
+        Me.OpenMenu.Size = New System.Drawing.Size(63, 13)
+        Me.OpenMenu.TabIndex = 15
+        Me.OpenMenu.Text = "Open Menu"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1155, 455)
+        Me.Controls.Add(Me.OpenMenu)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Red As Button
+    Friend WithEvents Orange As Button
+    Friend WithEvents Yellow As Button
+    Friend WithEvents LightGreen As Button
+    Friend WithEvents DarkGreen As Button
+    Friend WithEvents LightBlue As Button
+    Friend WithEvents DarkBlue As Button
+    Friend WithEvents Purple As Button
+    Friend WithEvents Black As Button
+    Friend WithEvents Eraser As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents OpenMenu As Label
+    Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents Line As Button
+    Friend WithEvents BrushButton As Button
+End Class
---
+++ Paint/Form1.vb
@@ -0,0 +1,131 @@
+﻿Public Class Form1
+    Dim down As Boolean
+    Dim brush As New SolidBrush(Color.White)
+    Dim thickness As Integer
+    Dim linestart As Point
+    Dim brushpressed As Boolean
+    Dim linepressed As Boolean
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+
+        Dim blackPen As New Pen(Color.Black, 3)
+
+        draw.DrawEllipse(blackPen, 175, 25, 75, 75)
+        draw.DrawLine(blackPen, 210, 100, 210, 210)
+        draw.DrawLine(blackPen, 210, 209, 140, 325)
+        draw.DrawLine(blackPen, 210, 209, 280, 325)
+        draw.DrawLine(blackPen, 210, 155, 125, 100)
+        draw.DrawLine(blackPen, 210, 155, 295, 100)
+
+        draw.DrawEllipse(blackPen, 600, 25, 75, 75)
+        draw.DrawLine(blackPen, 635, 100, 635, 210)
+        draw.DrawLine(blackPen, 635, 209, 565, 325)
+        draw.DrawLine(blackPen, 635, 209, 705, 325)
+        draw.DrawLine(blackPen, 635, 155, 550, 100)
+        draw.DrawLine(blackPen, 635, 155, 720, 100)
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim draw As System.Drawing.Graphics = Me.CreateGraphics
+        Dim X As Integer = Form1.MousePosition.X - Me.Location.X - 10
+        Dim Y As Integer = Form1.MousePosition.Y - Me.Location.Y - 35
+
+        If brushpressed = True And
+                down Then
+            draw.FillEllipse(brush, X, Y, thickness, thickness)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        down = True
+        Dim X As Integer = Form1.MousePosition.X - Me.Location.X - 10
+        Dim Y As Integer = Form1.MousePosition.Y - Me.Location.Y - 35
+
+        linestart = New Point(X, Y)
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        Dim draw As System.Drawing.Graphics = Me.CreateGraphics
+        Dim X As Integer = Form1.MousePosition.X - Me.Location.X - 10
+        Dim Y As Integer = Form1.MousePosition.Y - Me.Location.Y - 35
+        Dim lineEnd As Point
+        lineEnd = New Point(X, Y)
+        If linepressed = True Then
+            draw.DrawLine(New Pen(brush.Color, thickness), linestart, lineEnd)
+        End If
+        down = False
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Red_Click(sender As Object, e As EventArgs) Handles Red.Click
+        brush.Color = Color.Red
+    End Sub
+
+    Private Sub Orange_Click(sender As Object, e As EventArgs) Handles Orange.Click
+        brush.Color = Color.OrangeRed
+    End Sub
+
+    Private Sub Yellow_Click(sender As Object, e As EventArgs) Handles Yellow.Click
+        brush.Color = Color.Gold
+    End Sub
+
+    Private Sub LightGreen_Click(sender As Object, e As EventArgs) Handles LightGreen.Click
+        brush.Color = Color.YellowGreen
+    End Sub
+
+    Private Sub DarkGreen_Click(sender As Object, e As EventArgs) Handles DarkGreen.Click
+        brush.Color = Color.Green
+    End Sub
+
+    Private Sub LightBlue_Click(sender As Object, e As EventArgs) Handles LightBlue.Click
+        brush.Color = Color.Cyan
+    End Sub
+
+    Private Sub DarkBlue_Click(sender As Object, e As EventArgs) Handles DarkBlue.Click
+        brush.Color = Color.Blue
+    End Sub
+
+    Private Sub Purple_Click(sender As Object, e As EventArgs) Handles Purple.Click
+        brush.Color = Color.DarkViolet
+    End Sub
+
+    Private Sub Black_Click(sender As Object, e As EventArgs) Handles Black.Click
+        brush.Color = Color.Black
+    End Sub
+
+    Private Sub Eraser_Click(sender As Object, e As EventArgs) Handles Eraser.Click
+        brushpressed = True
+        brush.Color = Color.White
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+
+        draw.FillRectangle(Brushes.White, 0, 0, 1600, 850)
+    End Sub
+
+    Private Sub OpenMenu_Click(sender As Object, e As EventArgs) Handles OpenMenu.Click
+        Panel1.Visible = Not Panel1.Visible
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        thickness = HScrollBar1.Value
+    End Sub
+
+    Private Sub BrushButton_Click(sender As Object, e As EventArgs) Handles BrushButton.Click
+        brushpressed = True
+        linepressed = False
+    End Sub
+
+    Private Sub Line_Click(sender As Object, e As EventArgs) Handles Line.Click
+        linepressed = True
+        brushpressed = False
+    End Sub
+End Class
